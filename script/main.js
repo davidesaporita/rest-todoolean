1ef03fea667fff8049a313e699fd540ed7ddda02
Davide Saporita <davide.saporita@gmail.com>
2020-05-11 17:38:10 +0200

    JS | CRUD - "Delete" function added

--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
 // API Vars
-var $apiUrl = 'http://157.230.17.132:3019/todos';
-var settingsGET = { url: $apiUrl, method: 'GET' }
-var settingsPOST = { url: $apiUrl, method: 'POST', data: { text: '' } }
+var apiUrl = 'http://157.230.17.132:3019/todos';
+var settingsGET = { url: apiUrl, method: 'GET' }
+var settingsPOST = { url: apiUrl, method: 'POST', data: { text: '' } }
+var settingsDELETE = { url: apiUrl, method: 'DELETE' }
 
 $(document).ready(function() {
 
     // Refs
+    var app = $('#app');
     var list = $('.list-items');
     var input = $('.input-text');
 
@@ -20,6 +22,10 @@ $(document).ready(function() {
         if(e.which === 13 || e.keyCode === 13) {
             addToDo(input.val().trim(), template, list);
         }
+    });
+
+    app.on('click','.remove', function() {
+        deleteToDo($(this), template, list);    
     });
 
 }); // End of ready function
@@ -37,10 +43,21 @@ function printItems(data, template, ref) {
     data.forEach(item => { ref.append(template(item)); });
 }
 
+// CRud - Add new element
 function addToDo(newValue, template, list) {    
-    settingsPOST.data = { text : newValue }
-    
+    settingsPOST.data = { text: newValue };
     $.ajax(settingsPOST)
         .done(data  => { populate(template, list); })
         .fail(error => { console.log('fail' + error); });
+}
+
+// cruD - Delete element
+function deleteToDo(self, template, list) {
+    var selfId = self.parents('li.item').data('id');
+    settingsDELETE.url += '/' + selfId;
+
+    $.ajax(settingsDELETE)
+        .done(data  => { populate(template, list); })
+        .fail(error => { console.log('fail' + error); })
+        .always(()  => { settingsDELETE.url = apiUrl; });
 }
