e610976499f6418101e0eace565d4b0ff5c0a870
Davide Saporita <davide.saporita@gmail.com>
2020-05-11 17:14:54 +0200

    JS | CRUD - "Create" function added

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // API Vars
 var $apiUrl = 'http://157.230.17.132:3019/todos';
 var settingsGET = { url: $apiUrl, method: 'GET' }
-var settingsPOST = { url: $apiUrl, method: 'POST' }
+var settingsPOST = { url: $apiUrl, method: 'POST', data: { text: '' } }
 
 $(document).ready(function() {
 
@@ -15,17 +15,32 @@ $(document).ready(function() {
 
     populate(template, list);
 
+    // Event on keyup | input
+    input.keyup((e) => {
+        if(e.which === 13 || e.keyCode === 13) {
+            addToDo(input.val().trim(), template, list);
+        }
+    });
+
 }); // End of ready function
 
 /*** FUNCTIONS ***/
 
 function populate(template, list){
     $.ajax(settingsGET)
-    .done(data  => { printItems(data, template, list); })
-    .fail(error => { console.log('fail' + error); });
+        .done(data  => { printItems(data, template, list); })
+        .fail(error => { console.log('fail' + error); });
 }
 
 function printItems(data, template, ref) {
+    ref.html('');
     data.forEach(item => { ref.append(template(item)); });
 }
 
+function addToDo(newValue, template, list) {    
+    settingsPOST.data = { text : newValue }
+    
+    $.ajax(settingsPOST)
+        .done(data  => { populate(template, list); })
+        .fail(error => { console.log('fail' + error); });
+}
