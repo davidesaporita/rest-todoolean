5d5fc1ddec719cbf804ef72e8bcec847978e5f9b
Davide Saporita <davide.saporita@gmail.com>
2020-05-11 16:51:05 +0200

    JS | "Read" function added

--- script/main.js
+++ script/main.js
@@ -1,18 +1,31 @@
-/**
- * ToDoolean
- * 
- * Creazione di una todo list con le seguenti funzionalità, attraverso l’uso delle API, AJAX, jQuery e Handlebars
- * - Lettura di tutti i todo
- * - Creazione nuovo todo
- * - Cancellazione todo
- *  
- */
-
-$apiUrl = '157.230.17.132:3019/todos';
+// API Vars
+var $apiUrl = 'http://157.230.17.132:3019/todos';
+var settingsGET = { url: $apiUrl, method: 'GET' }
+var settingsPOST = { url: $apiUrl, method: 'POST' }
 
 $(document).ready(function() {
-    
 
+    // Refs
+    var list = $('.list-items');
+    var input = $('.input-text');
 
-    
+    // Init Handlebars
+    var source = $('#item-template').html();
+    var template = Handlebars.compile(source);    
+
+    populate(template, list);
+
 }); // End of ready function
+
+/*** FUNCTIONS ***/
+
+function populate(template, list){
+    $.ajax(settingsGET)
+    .done(data  => { printItems(data, template, list); })
+    .fail(error => { console.log('fail' + error); });
+}
+
+function printItems(data, template, ref) {
+    data.forEach(item => { ref.append(template(item)); });
+}
+
